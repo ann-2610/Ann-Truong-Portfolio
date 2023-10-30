865a70e3997dbef85cf1f218c7270c96faa1d706
ann-2610 <121140185+ann-2610@users.noreply.github.com>
2023-10-30 00:52:32 -0400

    UI Interaction Added

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
-$(".heroButton").on("mouseover", function() { 
-    $(".heroButton a").css('color', 'white');
+$(document).ready(function() {
+    // Hide the content initially
+    $('#content').hide();
+  
+    // Hide the preloader when the page is fully loaded
+    $(window).on('load', function() {
+        $('.preloader').fadeOut(1000, function() {
+            // Show the content after preloader is hidden
+            $('#content').show();
+        });
+    });
 });
+
+$(document).ready(function() {
+  $('.enlargeImage').hover(
+      function() {
+          $(this).css('transform', 'scale(1.1)'); // Enlarge the image on hover
+      },
+      function() {
+          $(this).css('transform', 'scale(1)'); // Restore the original size when not hovering
+      }
+  );
+});
+
+  
